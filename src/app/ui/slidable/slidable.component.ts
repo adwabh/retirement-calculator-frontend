d28307b680779564f69f094d79602c4993c6ce20
Adwait Abhyankar <avabhyankar22@gmail.com>
2022-11-26 16:02:47 +0530

    [RET-002] WIP changes to slider comp

--- src/app/ui/slidable/slidable.component.ts
+++ src/app/ui/slidable/slidable.component.ts
@@ -1,4 +1,6 @@
-import { Component, OnInit } from '@angular/core';
+import { Form, FormBuilder } from '@angular/forms';
+import { Component, Input, OnInit, Output, EventEmitter } from '@angular/core';
+import { FormGroup } from '@angular/forms';
 
 @Component({
   selector: 'ui-slidable',
@@ -7,9 +9,35 @@ import { Component, OnInit } from '@angular/core';
 })
 export class SlidableComponent implements OnInit {
 
+  @Input()
+  formBuilder: FormBuilder | null = null
+
+  public slidableInputForm: FormGroup | undefined;
+
+  @Input()
+  title = ''
+
+  @Output()
+  scaledValue = new EventEmitter<Number>()
+
+
+  _scaledValue = 0
+
   constructor() { }
 
   ngOnInit(): void {
+    this.slidableInputForm = this.buildForm(this.formBuilder)
+  }
+
+  buildForm = (formBuilder:FormBuilder) => {
+    return formBuilder.group({ } )
+   }
+
+  @Input()
+  formatLabel: (value:number) => number = (value : number) => this._scaledValue
+
+  onSlidableTextInput(event: Event) {
+
   }
 
 }
